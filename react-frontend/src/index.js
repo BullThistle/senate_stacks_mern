88789159cb72bff4551b0c26d28b68ce4dbf94b0
Rafael Furry <rfurry@gmail.com>
2018-03-26 12:38:32 -0700

    update index.js

--- react-frontend/src/index.js
+++ react-frontend/src/index.js
@@ -14,21 +14,15 @@ import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 
 ReactDOM.render(
-  <div className="container">
-    <div className="row">
-      <div className="col-xs-12">
-        <Header/>
-      </div>
-    </div>
-    <div>
-      <Router>
+  <div>
+    <Header/>
+    <Router>
       <div>
-      <Route path='/add' component={AddTopic} />
-      <Route exact path='/' component={ListTopic} />
-      <Route path='/update/:id' component={UpdateTopic} />
+        <Route path='/add' component={AddTopic} />
+        <Route exact path='/' component={ListTopic} />
+        <Route path='/update/:id' component={UpdateTopic} />
       </div>
-      </Router>
-    </div>
+    </Router>
   </div>
 ,
   document.getElementById('root')
