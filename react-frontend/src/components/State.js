99f98892be08a4f877d6351515781bb6f50d0069
Rafael Furry <rfurry@gmail.com>
2018-03-26 13:09:29 -0700

    create state component

---
+++ react-frontend/src/components/State.js
@@ -0,0 +1,16 @@
+
+import React, { Component } from 'react';
+import { Container } from 'semantic-ui-react'
+
+export default class State extends Component {
+
+  render() {
+    return (
+      <Container style={{ marginTop: '7em' }}>
+        <Grid className="centered">
+          
+        </Grid>
+      </Container>
+    );
+  }
+}
